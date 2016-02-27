721388a3833ffc248a737719e69cc4d0281c8782
TheDooner64 <Robert.Muldoon.1@gmail.com>
2016-02-27 02:29:57 -0500

    Save copies of widgets to database for forked dashboards

--- server/app/routes/dashboards/index.js
+++ server/app/routes/dashboards/index.js
@@ -124,7 +124,7 @@ router.delete("/:id", ensureAuthenticated, function(req, res, next) {
 
 // /api/dashboards/id/widgets
 router.get("/:id/widgets", function (req, res, next) {
-    Widget.find({dashboard: req.params.id})
+    Widget.find({ dashboard: req.params.id })
     .then(widgets => res.status(201).send(widgets))
     .then(null, function(err) {
         err.message = "Something went wrong when trying to access this widget";
@@ -137,18 +137,40 @@ router.get("/:id/widgets", function (req, res, next) {
 router.post("/:dashboardId/fork", ensureAuthenticated, function(req, res, next) {
     // Create a dashboard for the new user
     var dashboardToFork = req.body,
-    datasetId = dashboardToFork.dataset._id;
+    dashboardToForkId = dashboardToFork._id,
+    forkedDashboard,
+    clonedWidgets = [];
+
+    // Make sure the dashboard being forked is public
+    if (!dashboardToFork.isPublic) res.status(401).send("You are not authorized to access this dashboard");
 
     // Cleanse the original dashboard so a fresh one can be created in Mongo
     delete dashboardToFork._id;
     delete dashboardToFork.__v;
     dashboardToFork.user = req.user._id;
 
-    // Make sure the dashboard being forked is public
-    if (!dashboardToFork.isPublic) res.status(401).send("You are not authorized to access this dashboard");
-
+    // Create a new dashboard in mongo
     Dashboard.create(dashboardToFork)
-    .then(forkedDashboard => res.status(201).json(forkedDashboard))
+    .then(forkedDashboardFromMongo => {
+        forkedDashboard = forkedDashboardFromMongo;
+        // Find the widgets associated with the forked dashboard
+        return Widget.find({ dashboard: dashboardToForkId });
+    })
+    .then(widgetsToClone => {
+        // Create "cleansed" copies of all associated widgets
+        widgetsToClone.forEach(widgetToClone => {
+            let clonedWidget = Object.assign({}, widgetToClone.toJSON());
+            delete clonedWidget._id;
+            delete clonedWidget.__v;
+            clonedWidget.user = req.user._id;
+            clonedWidget.dashboard = forkedDashboard._id;
+            clonedWidgets.push(clonedWidget);
+        });
+        return Widget.create(clonedWidgets);
+    })
+    .then(forkedWidgets => {
+        res.status(201).json(forkedDashboard);
+    })
     .then(null, function(err) {
         err.message = "Something went wrong when trying to fork this dashboard";
         next(err);
